10b860652c54f2755f6f7e3c4c764ed24f8d6eca
Mukesh <billamukesh1234@gmail.com>
2024-07-20 23:04:25 +0530

    add: toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <Navbar />
           {children}
+          <Toaster/>
         </body>
       </html>
     </ClerkProvider>
