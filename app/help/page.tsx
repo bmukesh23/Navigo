7903d6a42f89ad0b727c2b2c464e53f473e2f95d
Mukesh <billamukesh1234@gmail.com>
2024-07-21 22:50:57 +0530

    style: add margin bottom in help component

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -1,8 +1,6 @@
-import Link from "next/link"
-
 const Help = () => {
   return (
-    <section className="mt-10 px-2 lg:w-1/2 mx-auto">
+    <section className="mt-10 px-2 lg:w-1/2 mx-auto mb-8">
         <h2 className="flex justify-center items-center font-semibold text-lg lg:text-2xl text-center">Welcome to Navigo! Get started with these simple steps:</h2>
         <div className="flex flex-col justify-center items-center text-justify w-[70%] mx-auto mt-8 gap-4">
             <p className="font-semibold">Install Metamask: <span className="font-normal">Install and Add the MetaMask extension to your browser from the MetaMask website.</span></p>
