1b3ab1e6dfcaaf6831844663f72479c59f145518
Mukesh <billamukesh1234@gmail.com>
2024-12-22 16:33:19 +0530

    style: updated home ui

--- components/home.tsx
+++ components/home.tsx
@@ -1,15 +1,33 @@
+import { useState, useEffect } from "react"
 import { SignInButton } from "@clerk/nextjs"
+import Loader from "./loader";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 const Home = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <section className="flex flex-col justify-center items-center">
-            <div className="bg-blue-600 absolute top-[-6rem] -z-10 right-[11rem] h-[20rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
             <h2 className="text-4xl sm:text-5xl lg:text-6xl font-bold pt-52 leading-tight mx-auto text-center bg-gradient-to-b from-white to-blue-950 text-transparent bg-clip-text">Ride the blockchain revolution</h2>
             <p className="text-xs md:text-base w-[95%] md:w-[80%] lg:w-[40%] px-2 md:px-0 text-center">Navigo is a blockchain-enabled cab booking app that ensures safe and transparent transactions using MetaMask.</p>
-            <div className="flex justify-center bg-blue-950 text-white font-semibold text-sm p-2 rounded-md w-[8rem] md:w-[10rem] mx-auto mt-4 hover:cursor-pointer hover:bg-slate-800">
-                <SignInButton>
-                    <p className="text-xs md:text-base">Get Started</p>
-                </SignInButton>
+            <div className="flex justify-center bg-blue-950 text-white text-sm p-2 rounded-md w-[8rem] md:w-[10rem] mx-auto mt-4 hover:cursor-pointer hover:bg-blue-900">
+                {loading ? (
+                    <Loader />
+                ) : (
+                    <SignInButton>
+                        <div className="flex items-center">
+                            <p className="text-xs md:text-base">Get Started</p>
+                            <IoIosArrowRoundForward className="ml-1 text-lg sm:text-2xl"/>
+                        </div>
+                    </SignInButton>
+                )}
             </div>
         </section>
     )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,32 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+  	extend: {
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+  		},
+  		animation: {
+  			ripple: 'ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite'
+  		},
+  		keyframes: {
+  			ripple: {
+  				'0%, 100%': {
+  					transform: 'translate(-50%, -50%) scale(1)'
+  				},
+  				'50%': {
+  					transform: 'translate(-50%, -50%) scale(0.9)'
+  				}
+  			}
+  		}
+  	}
   },
   plugins: [],
 };
